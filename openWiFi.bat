9bf75893d05db23c7bfa2ceb774a3b3b05fa45cb
wudan <wudan@shinezone.com>
2016-06-02 10:42:22 +0800

    ! ssid name

--- openWiFi.bat
+++ openWiFi.bat
@@ -1,4 +1,4 @@
 netsh wlan stop hostednetwork
 netsh wlan set hostednetwork mode=all
-netsh wlan set hostednetwork ssid=LHC key=12345678
+netsh wlan set hostednetwork ssid=marz key=12345678
 netsh wlan start hostednetwork
